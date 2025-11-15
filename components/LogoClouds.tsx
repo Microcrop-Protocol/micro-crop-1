24b2ebf3064385c368966bd65702e44d16ba0dce
Zeegaths <zarahgathoni76@gmail.com>
2025-11-15 04:13:13 +0300

    logos update

--- components/LogoClouds.tsx
+++ components/LogoClouds.tsx
@@ -5,11 +5,11 @@ const LogoClouds = () => {
   return (
     <div className="bg-[#1D5B37] py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <h2 className="text-center text-lg/8 font-semibold text-white">Trusted by the world’s most innovative teams</h2>
+        <h2 className="text-center text-lg/8 font-semibold text-white">Built on tech you can trust</h2>
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
           <Image
             alt="Transistor"
-            src="https://tailwindcss.com/plus-assets/img/logos/158x48/transistor-logo-white.svg"
+            src="/img/flow.png"
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
@@ -17,7 +17,7 @@ const LogoClouds = () => {
 
           <Image
             alt="Reform"
-            src="https://tailwindcss.com/plus-assets/img/logos/158x48/reform-logo-white.svg"
+            src="/img/Base.png"
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
@@ -25,7 +25,7 @@ const LogoClouds = () => {
 
           <Image
             alt="Tuple"
-            src="https://tailwindcss.com/plus-assets/img/logos/158x48/tuple-logo-white.svg"
+            src="/img/xm-.png"
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
@@ -33,7 +33,7 @@ const LogoClouds = () => {
 
           <Image
             alt="SavvyCal"
-            src="https://tailwindcss.com/plus-assets/img/logos/158x48/savvycal-logo-white.svg"
+            src="/img/mpesa.png"
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
@@ -41,7 +41,7 @@ const LogoClouds = () => {
 
           <Image
             alt="Statamic"
-            src="https://tailwindcss.com/plus-assets/img/logos/158x48/statamic-logo-white.svg"
+            src="/img/plgenesis.jpg"
             width={158}
             height={48}
             className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"

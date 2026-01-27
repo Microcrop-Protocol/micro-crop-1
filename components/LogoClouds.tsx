4b637ccb6dbd0ec7a1a3c71ebcbcaf0573267336
Antismart <timbwamoses83@gmail.com>
2026-01-27 22:12:35 +0300

    refactor: rebrand logo cloud as technology stack section
    Change heading from "Built on tech you can trust" to "Our Technology
    Stack" and update image alt text to match actual partner names.

--- components/LogoClouds.tsx
+++ components/LogoClouds.tsx
@@ -5,10 +5,10 @@ const LogoClouds = () => {
   return (
     <div className="bg-[#1D5B37] py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <h2 className="text-center text-lg/8 font-semibold text-white">Built on tech you can trust</h2>
+        <h2 className="text-center text-lg/8 font-semibold text-white">Our Technology Stack</h2>
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
           <Image
-            alt="Transistor"
+            alt="Flow"
             src="/img/flow.png"
             width={158}
             height={48}
@@ -16,7 +16,7 @@ const LogoClouds = () => {
           />
 
           <Image
-            alt="Reform"
+            alt="Base"
             src="/img/Base.png"
             width={158}
             height={48}
@@ -24,7 +24,7 @@ const LogoClouds = () => {
           />
 
           <Image
-            alt="Tuple"
+            alt="WeatherXM"
             src="/img/xm-.png"
             width={158}
             height={48}
@@ -32,7 +32,7 @@ const LogoClouds = () => {
           />
 
           <Image
-            alt="SavvyCal"
+            alt="M-Pesa"
             src="/img/mpesa.png"
             width={158}
             height={48}
@@ -40,7 +40,7 @@ const LogoClouds = () => {
           />
 
           <Image
-            alt="Statamic"
+            alt="PLGenesis"
             src="/img/plgenesis.jpg"
             width={158}
             height={48}

e6beb9f018aacd819632be3582b42ea4100df197
Antismart <timbwamoses83@gmail.com>
2026-03-04 23:46:55 +0300

    fix: replace Flow logo with Chainlink and improve mobile grid layout

--- components/LogoClouds.tsx
+++ components/LogoClouds.tsx
@@ -6,13 +6,13 @@ const LogoClouds = () => {
     <div className="bg-[#1D5B37] py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <h2 className="text-center text-lg/8 font-semibold text-white">Our Technology Stack</h2>
-        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-4 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-4">
+        <div className="mx-auto mt-10 grid max-w-lg grid-cols-2 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-4 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-4">
           <Image
-            alt="Flow"
-            src="/img/flow.png"
+            alt="Chainlink"
+            src="/img/chainlink.png"
             width={158}
             height={48}
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="max-h-12 w-full object-contain"
           />
 
           <Image
@@ -20,7 +20,7 @@ const LogoClouds = () => {
             src="/img/Base.png"
             width={158}
             height={48}
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="max-h-12 w-full object-contain"
           />
 
           <Image
@@ -28,7 +28,7 @@ const LogoClouds = () => {
             src="/img/xm-.png"
             width={158}
             height={48}
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            className="max-h-12 w-full object-contain"
           />
 
           <Image
@@ -36,7 +36,7 @@ const LogoClouds = () => {
             src="/img/mpesa.png"
             width={158}
             height={48}
-            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+            className="max-h-12 w-full object-contain"
           />
         </div>
       </div>

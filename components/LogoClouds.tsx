2a7bc99f8721c2bacb4a29b33d502b5e802b7347
Antismart <timbwamoses83@gmail.com>
2026-04-15 08:43:38 +0300

    fix: remove M-Pesa from landing page technology stack logos

--- components/LogoClouds.tsx
+++ components/LogoClouds.tsx
@@ -5,7 +5,7 @@ const LogoClouds = () => {
     <div className="bg-[#1D5B37] py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <h2 className="text-center text-lg/8 font-semibold text-white">Our Technology Stack</h2>
-        <div className="mx-auto mt-10 grid max-w-lg grid-cols-2 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-4 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-4">
+        <div className="mx-auto mt-10 grid max-w-lg grid-cols-3 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:gap-x-10 lg:mx-0 lg:max-w-3xl">
           <Image
             alt="Chainlink"
             src="/img/chainlink.png"
@@ -25,14 +25,6 @@ const LogoClouds = () => {
           <Image
             alt="WeatherXM"
             src="/img/xm-.png"
-            width={158}
-            height={48}
-            className="max-h-12 w-full object-contain"
-          />
-
-          <Image
-            alt="M-Pesa"
-            src="/img/mpesa.png"
             width={158}
             height={48}
             className="max-h-12 w-full object-contain"
